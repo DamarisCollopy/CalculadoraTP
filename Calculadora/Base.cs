1e0b1df3134a8fdf3bfa40a8f49b350ac2d9bcb1
Damaris-PC <damisoliveira@gmail.com>
2020-08-07 16:15:48 -0300

    Projeto de Bloco entrega

---
+++ Calculadora/Base.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Calculadora
+{
+    class Base
+    {
+        private double soma, divisao, multiplicar, subtracao;
+
+        public double Soma { get; set; }
+        public double Subtracao { get; set; }
+        public double Multiplicar { get; set; }
+        public double Divisao { get; set; }
+
+
+        public double resultadoSoma(double x, double y)
+        {
+            return soma = x + y;
+        }
+        public double resultadoDivisao()
+        {
+            double op1;
+            double op2;
+
+            do
+            {
+                Console.Write("Informe o op1 = ");
+                op1 = double.Parse(Console.ReadLine());
+                Console.Write("Informe o op2 = ");
+                op2 = double.Parse(Console.ReadLine());
+
+                if ((op1 <= 0) || (op2 <= 0))
+                {
+                    Console.WriteLine("Operação não é possivel ser realizada");
+                    Console.ReadLine();
+                }
+            }
+            while ((op1 <= 0) || (op2 <= 0));
+            return divisao = op1 / op2;
+        }
+
+        public double resultadoMultiplicar()
+        {
+            double op1;
+            double op2;
+            do
+            {
+                Console.Write("Informe o op1 = ");
+                op1 = double.Parse(Console.ReadLine());
+                Console.Write("Informe o op2 = ");
+                op2 = double.Parse(Console.ReadLine());
+                if ((op1 <= 0) && (op2 <= 0))
+                {
+                    Console.WriteLine("Operação não é possivel ser realizada");
+                    Console.ReadLine();
+                }
+            }
+            while ((op1 <= 0) && (op2 <= 0));
+            return multiplicar = op1 * op2;
+        }
+
+        public double resultadoSubtrair(double op1, double op2)
+        {
+            return subtracao = op1 - op2;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base calculadora = new Base();
+            int SAIR = 5;
+            int opcao = Menu();
+
+            while (opcao != SAIR)
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Informe o x = ");
+                        double op1 = double.Parse(Console.ReadLine());
+                        Console.Write("Informe o y = ");
+                        double op2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Resultado da Soma, {calculadora.resultadoSoma(op1, op2)}.");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Resultado da Multiplicação, {calculadora.resultadoDivisao()}.");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine($"Resultado da Multiplicação, {calculadora.resultadoMultiplicar()}.");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Informe o x = ");
+                        op1 = double.Parse(Console.ReadLine());
+                        Console.Write("Informe o y = ");
+                        op2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Resultado da Subtra, { calculadora.resultadoSubtrair(op1, op2)}.");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Programa Encerrado!");
+                        Console.ReadLine();
+                        break;
+                }
+                opcao = Menu();
+            }
+        }
+
+        static int Menu()
+        {
+            int opcao;
+            Console.Clear();
+            Console.WriteLine("Calculadora:");
+            Console.WriteLine("1) Soma");
+            Console.WriteLine("2) Divisao");
+            Console.WriteLine("3) Multiplicar");
+            Console.WriteLine("4) Subtrair");
+            Console.WriteLine("5) Exit");
+            Console.Write("\r\nSelecione a opção: ");
+            do
+            {
+                opcao = int.Parse(Console.ReadLine());
+                if ((opcao < 1) || (opcao > 5))
+                {
+                    Console.WriteLine("Desculpe opção invalida");
+                    Console.ReadLine();
+                    return opcao;
+                }
+            }
+            while ((opcao <= 0) || (opcao > 5));
+            return opcao;
+        }
+
+    
+    }
+}
